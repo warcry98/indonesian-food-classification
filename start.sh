99a84fe9d89db6ec9900d21e31d3ae6ad38986f7
Wahid Sholihul Fikri <wahidsf1905@gmail.com>
2026-01-18 00:28:58 +0700

    Modified: start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
 
-uvicorn backend.api:app --host 0.0.0.0 --port 8000
-streamlit run app.py
+uvicorn backend.api:app --host 0.0.0.0 --port 8000 &
+UVICORN_PID=$!
+
+streamlit run app.py &
+STREAMLIT_PID=$!
+
+trap "kill $UVICORN_PID $STREAMLIT_PID" SIGTERM SIGINT
+
+wait $UVICORN_PID $STREAMLIT_PID
